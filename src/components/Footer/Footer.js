2ccab5b4c3242bf649ca00bcb98d999488296bd7
azmul-ml <azmul.hossain@.monstar-lab.com>
2022-03-01 10:44:50 +0600

    Initial Commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <span className="username">Cypress Testing Boilerplate</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const FACEBOOK_AUTH_LINK = 'https://mern-boilerplate-demo.herokuapp.com/auth/facebook';
+export const GOOGLE_AUTH_LINK = 'https://mern-boilerplate-demo.herokuapp.com/auth/google';
